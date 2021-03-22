db9d061c7258d9e66ae17a3a149f4ed2ecacdaac
Oliver Wilkinson <o.m.wilkinson@2019.ljmu.ac.uk>
2021-03-22 14:30:34 +0000

    .

--- OOSystems/src/sys.java
+++ OOSystems/src/sys.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class sys {
 	private final static Scanner S = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		
 		String choice = "";
 		
