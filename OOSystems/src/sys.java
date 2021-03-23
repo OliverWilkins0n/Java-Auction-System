bd29ba0e9b010a580239a8876c3948daea3eba08
OliverWilkins0n <o.m.wilkinson@2019.ljmu.ac.uk>
2021-03-23 13:38:42 +0000

    .

--- OOSystems/src/sys.java
+++ OOSystems/src/sys.java
@@ -4,9 +4,9 @@ public class sys {
 	private final static Scanner S = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+
 		String choice = "";
-		
+
 		do {
 			System.out.println("==== Main Menu ====");
 			System.out.println("1 - Place an Auction");
@@ -14,36 +14,39 @@ public class sys {
 			System.out.println("3 - Setup Account");
 			System.out.println("Q - Quit");
 			System.out.println("Input: ");
-			
+
 			choice = S.nextLine().toUpperCase();
-			
+
 			switch (choice) {
 				case "1" : {
 					placeAuction();
+					break;
 				}
 				case "2" : {
 					browseAuction();
+					break;
 				}
 				case "3" : {
 					setupAccount();
+					break;
 				}
 			}
-			
+
 		} while(!choice.equals("Q"));
 
 	}
 
-	
+
 	public static void placeAuction() {
-		
+
 	}
-	
+
 	public static void browseAuction() {
-		
+
 	}
-	
+
 	public static void setupAccount() {
-		
+
 	}
-	
+
 }
