a4f943edf5f4af7ebf31732810e3361dfebbec94
arfla <46490532+arfla@users.noreply.github.com>
2021-03-24 13:44:19 +0000

    Setting up auction database

--- OOSystems/src/sys.java
+++ OOSystems/src/sys.java
@@ -5,13 +5,13 @@ import java.io.FileWriter;
 import java.util.Scanner;
 
 public class sys {
-	//private List<User> users = new LinkedList<User>();
 	private final static Scanner S = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
 		String choice = "";
 		String filepath = "database.txt";
+		String filepath_auction = "auction_database.txt";
 
 		do {
 			//Doesnt a user need to login to place an auction?
@@ -28,7 +28,7 @@ public class sys {
 			switch (choice) {
 				case "1" :
 				case "P" :{
-					placeAuction();
+					placeAuction(filepath_auction);
 					break;
 				}
 				case "2" :
@@ -49,11 +49,36 @@ public class sys {
 			}
 
 		} while(!choice.equals("Q"));
-
+		//close Scanner
+		S.close();
+		System.out.println("Goodbye :)");
 	}
 
+	public static void placeAuction(String filepath_auction) {
+		//just add to it, I can't think off anymore right now
+		System.out.print("Please enter a item name: ");
+		String itemName = S.next();
+		System.out.print("Please enter a description: ");
+		String description = S.next();
+		System.out.print("Please enter a start price: ");
+		Double startPrice = S.nextDouble();
+		System.out.print("Please enter a reserve price: ");
+		Double reservePrice = S.nextDouble();
+		try {
+			FileWriter fw2 = new FileWriter(filepath_auction,true);
+			BufferedWriter bw2 = new BufferedWriter(fw2);
+			PrintWriter pw2 = new PrintWriter(bw2);
+			//writing username and password into new line of our database
+			pw2.println(itemName+","+description+","+startPrice+","+reservePrice);
+			pw2.flush();
+			pw2.close();
 
-	public static void placeAuction() {
+			JOptionPane.showMessageDialog(null, "Auction successfully created!");
+		}
+		catch (Exception E)
+		{
+			JOptionPane.showMessageDialog(null, "Auction could not be created, please try again!");
+		}
 
 	}
 
@@ -62,15 +87,17 @@ public class sys {
 	}
 
 	public static void setupAccount(String filepath) {
+		//need to add to check if username already exists
 		System.out.print("Please enter a username: ");
 		String username = S.next();
+		//We need to add/link our password verification
 		System.out.print("Please enter a password: ");
 		String password = S.next();
 		try {
 			FileWriter fw = new FileWriter(filepath,true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw);
-
+			//writing username and password into new line of our database
 			pw.println(username+","+password);
 			pw.flush();
 			pw.close();
